d0f64c551bdc8229ba1fb107580fba48807ee806
Glaucia <Glucia Andrade>
2022-01-05 20:26:06 -0300

    Sistema cadastro de peças

---
+++ pecas.js
@@ -0,0 +1,34 @@
+// Variáveis,sao espaços na memória para guardar algum valor
+let pesoPeca  = 100
+
+// pesoPeca - Camel Case
+// peso_peca - Snake Case
+// PesoPeca - Pascal Case
+
+if(pesoPeca >100) {
+    console.log("Peso maior que 100g, logo,podemos cadastrar peca")
+} else {
+    console.log("Peso insuficiente, nao é possível cadastrar")
+}
+
+let numeroPecas = 10
+
+if (numeroPecas < 10) {
+    console.log("Ainda há espaço na caixa, podemos cadastrar a peça")
+} else {
+    console.log("Não há espaço disponível na caixa")
+}
+
+
+let nomePeca  = "Di"
+
+  console.log("O comprimento do nome da peça é:", nomePeca.length)
+  if(nomePeca.length < 3) {
+  console.log("Nome de peça muito curto, não é possível cadastrar")
+
+
+}  else {
+    console.log("Nome de peça adequado, podemos cadastrar")
+}
+
+  
